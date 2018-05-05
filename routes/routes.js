35a8124d5cc442e0427cf76e6b8b30cbbf8a80aa
Vitaly <vitaly@Orens-MacBook-Air.local>
2018-05-05 17:47:10 +0300

    it works

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,23 @@
-//var creds = { username : "username", password : "pass"}
-var username = 'username';
-var password = 'pass';
+var credentials = {sudo : '1111', admin : '1234', user : "4321"};
 
 var appRouter = function (app) {
+
     app.get("/", function(req, res) {
+
       var creds = req.query;
-      console.log("------------- " + JSON.stringify(creds.id));
-      if (username == creds.username && password == creds.password)
-        res.status(200).send("blabla");
-      else
-        res.status(401).send('');
+      var num = Math.floor((Math.random() * 5) + 1);
+
+      if(creds.password && creds.username){
+        if(credentials[creds.username]== creds.password){
+          console.log("-----username = " + creds.username + " pass: " + creds.password)
+          res.status(200).send("flip_" + num +"_times");
+        } else {
+          console.log("-----credentials[creds.username] = " + credentials[creds.username])
+          res.status(401).send('Unauthorized');
+        }
+      } else {
+        res.status(400).send('Error');
+      }
     });
   }
 
