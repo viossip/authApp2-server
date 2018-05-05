c546a2e8edde4a58f70bcb525e7756e1aa519aae
Vitaly <vitaly@Orens-MacBook-Air.local>
2018-05-05 21:21:39 +0300

    authApp2 server is ready

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,24 @@
-var credentials = {sudo : '1111', admin : '1234', user : "4321"};
+var permitted = { sudo : '1111', admin : '1234', username : "password" };
+
+var clients = { 000000000000000 : "1234:2" ,
+                359457078472619 : "1111:3" } ; // etc.
 
 var appRouter = function (app) {
 
     app.get("/", function(req, res) {
 
-      var creds = req.query;
+      var requestData = req.query;
       var num = Math.floor((Math.random() * 3) + 1);
 
-      if(creds.password && creds.username){
-        if(credentials[creds.username]== creds.password){
-          res.status(200).send("flip_" + num +"_times");
+      if(requestData.password && requestData.username && requestData.id){
+        if(permitted[requestData.username] == requestData.password && clients[requestData.id]){
+            var pass = clients[requestData.id].split(':');
+            res.status(200).send(requestData.id + ":" + pass[0] + ":" + pass[1]);
         } else {
           res.status(401).send('Unauthorized');
         }
       } else {
-        res.status(400).send('Error');
+        res.status(400).send('Data miss');
       }
     });
   }
